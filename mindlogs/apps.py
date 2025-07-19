d4c1f04f34bfa36f8f088ba0b7453558800b1fc6
Adithya-Balan <adithyabalan05@gmail.com>
2025-07-19 16:42:08 +0530

    Resolved issue for deletion in R2 for Snapshot(log)

--- mindlogs/apps.py
+++ mindlogs/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MindlogsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'mindlogs'
+    
+    def ready(self):
+        import mindlogs.signals
