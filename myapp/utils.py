830eba5ad05ffbfb02073f124c05f86198259020
Adithya-Balan <adithyabalan05@gmail.com>
2025-07-20 12:13:51 +0530

    Turned on email notification again

--- myapp/utils.py
+++ myapp/utils.py
@@ -11,18 +11,17 @@ verified_user_ids = EmailAddress.objects.filter(verified=True).values_list('user
 def send_notification_email(user, message, link=None):
     subject = "🔔 You have a new notification on DevMate"
     body = f"{message}\n\nCheck it out here: {link or 'https://www.devmate.space/notification/'}"
-    pass
     
-    # def send_async():
-    #     send_mail(
-    #         subject,
-    #         body,
-    #         settings.EMAIL_HOST_USER,
-    #         [user.user.email],
-    #         fail_silently=True,
-    #     )
+    def send_async():
+        send_mail(
+            subject,
+            body,
+            settings.EMAIL_HOST_USER,
+            [user.user.email],
+            fail_silently=True,
+        )
         
-    # threading.Thread(target=send_async).start()
+    threading.Thread(target=send_async).start()
     
 def notify_user(userinfo_receiver, userinfo_sender, obj, notif_type):
     content_type = ContentType.objects.get_for_model(obj)
